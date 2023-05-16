aff4f78001b897af5fdabe5374bb94d465627e93
shaole <2728039823@qq.com>
2023-05-16 22:58:13 +0800

    finish 24

---
+++ 02-链表/[19]删除链表的倒数第 N 个结点.cpp
@@ -0,0 +1,72 @@
+//给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,2,3,4,5], n = 2
+//输出：[1,2,3,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1], n = 1
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1,2], n = 1
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中结点的数目为 sz 
+// 1 <= sz <= 30 
+// 0 <= Node.val <= 100 
+// 1 <= n <= sz 
+// 
+//
+// 
+//
+// 进阶：你能尝试使用一趟扫描实现吗？ 
+//
+// Related Topics 链表 双指针 👍 2536 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummyHead=new ListNode(0,head);
+        ListNode* first=dummyHead;
+        ListNode* second=dummyHead;
+        for(int i=0;i<n;i++){
+            second=second->next;
+        }
+        while(second->next!= nullptr){
+            first=first->next;
+            second=second->next;
+        }
+        first->next=first->next->next;
+        return dummyHead->next;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
