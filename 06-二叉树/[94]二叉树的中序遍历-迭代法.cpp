ca80e44236c0432c28808c3b9395110bdefa622c
shaole <2728039823@qq.com>
2023-06-05 10:21:22 +0800

    finish 94-迭代法

---
+++ 06-二叉树/[94]二叉树的中序遍历-迭代法.cpp
@@ -0,0 +1,75 @@
+//给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [1,null,2,3]
+//输出：[1,3,2]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = [1]
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 100] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+//
+// Related Topics 栈 树 深度优先搜索 二叉树 👍 1831 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode *root) {
+        vector<int>res;
+        if(root== nullptr)return res;
+        stack<TreeNode*>st;
+        TreeNode*cur=root;
+        while(cur!= nullptr||!st.empty()){
+            if(cur!= nullptr){
+                st.push(cur);
+                cur=cur->left;
+            } else{
+                cur=st.top();
+                st.pop();
+                res.push_back(cur->val);
+                cur=cur->right;
+            }
+        }
+        return res;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
